7de7890bfbda42737161e05fff1c6819298cf16f
kca2250 <2250katok@gmail.com>
2022-11-20 11:57:13 +0900

    404ページ作成

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+import { Center, Flex, Heading } from "@chakra-ui/react";
+
+export const NotFoundPage: React.FC = () => (
+  <Center m="auto" height="100vh">
+    <Heading as="h3" size="md">
+      emtodo - 404 | Page Not Found.
+    </Heading>
+  </Center>
+);

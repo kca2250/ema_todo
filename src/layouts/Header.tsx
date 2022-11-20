e84f9a6d6add4a298cf7a7d38726652dd36c46ea
kca2250 <2250katok@gmail.com>
2022-11-20 23:32:20 +0900

    userのavatarとdrawerを開いてログアウトできる処理を追加

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,28 +1,92 @@
-import { Flex, Heading, Avatar } from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  Avatar,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  Input,
+  DrawerFooter,
+  Button,
+  useDisclosure,
+  Text,
+  Divider,
+} from "@chakra-ui/react";
+import { useRef } from "react";
 import { AddButton } from "../components/AddButton";
+import { auth } from "../firebase/config";
+import { useAuth } from "../firebase/utils/auth";
 
-export const Header: React.FC<{ name: string; photoURL: string }> = (props) => (
-  <header
-    style={{
-      width: "100vw",
-      height: "auto",
-      padding: "6px 8px",
-      borderBottom: "1px solid rgb(231 231 231)",
-    }}
-  >
-    <Flex
-      width="100%"
-      height="100%"
-      justifyContent="space-between"
-      alignItems="center"
+export const Header: React.FC = () => {
+  const user = auth.currentUser;
+  const name = user?.displayName as string;
+  const photoUrl = user?.photoURL as string;
+  const email = user?.email as string;
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { logout } = useAuth();
+  const btnRef = useRef(null);
+
+  return (
+    <header
+      style={{
+        width: "100vw",
+        height: "auto",
+        padding: "6px 8px",
+        borderBottom: "1px solid rgb(231 231 231)",
+      }}
     >
-      <Heading as="h1" size="md">
-        ema todo
-      </Heading>
-      <Flex gap="4" alignItems="center">
-        <AddButton />
-        <Avatar size="sm" name={props.name} src={props.photoURL} />
+      <Flex
+        width="100%"
+        height="100%"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Heading as="h1" size="md">
+          ema todo
+        </Heading>
+        <Flex gap="4" alignItems="center">
+          <AddButton />
+          <Avatar size="sm" name={name} src={photoUrl} onClick={onOpen} />
+        </Flex>
       </Flex>
-    </Flex>
-  </header>
-);
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader />
+
+          <DrawerBody>
+            <Text mt="10px">
+              <code style={{ backgroundColor: "whitesmoke" }}>{email}</code>
+              でログイン中
+            </Text>
+          </DrawerBody>
+          <Divider />
+          <DrawerFooter>
+            <Button
+              minW="100%"
+              colorScheme="red"
+              variant="outline"
+              mr={3}
+              onClick={() => {
+                logout();
+                onClose();
+              }}
+            >
+              ログアウト
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </header>
+  );
+};
